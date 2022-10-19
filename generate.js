34a78c7ae5129b057e042cc30d81b4d91e896285
c-asm <c-asm-coder@proton.me>
2022-10-19 16:56:47 +0300

    Initial commit

---
+++ generate.js
@@ -0,0 +1,99 @@
+class PanelItem {
+    constructor(type, direction = null, children = [], params = {}) {
+        this.type = type;
+        this.direction = direction;
+        this.children = children;
+        this.params = params;
+    }
+}
+var _old = PanelItem;
+PanelItem = function(...args) { return new _old(...args) };
+
+
+markup = [
+    PanelItem('body', 'column', [
+        PanelItem('', 'column', [
+            PanelItem('', 'row', [
+                PanelItem('', 'column', [
+                    PanelItem('text', '', [], { 'text': 'Test area', 'underline': true, 'bold': true, 'fontSize': '18px' }),
+                    PanelItem('button', '', [], { 'onclick': "alert('Test')", 'text': 'Show alert' }),
+                    PanelItem('button', '', [], { 'onclick': "highlight($('.htmlpanel'), 3)", 'text': 'Glow panel' }),
+                    PanelItem('checkmark', '', [], { 'onclick': "console.log('New checkmark = ', this.checked)", 'text': 'Check test' })
+                ], { 'border': 'right' }),
+                PanelItem('', 'column', [
+                    PanelItem('text', '', [], { 'text': 'This window', 'underline': true, 'bold': true, 'fontSize': '18px', }),
+                    PanelItem('checkbutton', '', [], { 'onclick': "minimizeWindow($('.htmlpanel')[0])", 'text': 'Minimize' }),
+                    PanelItem('input', '', [], { 'onclick': "titleWindow($('.htmlpanel')[0], this.value)", 'text': 'Title' })
+                ])
+            ])
+        ])
+    ], { 'panel_width': '300px' })
+]
+
+panel_element = $('.htmlpanel')[0]
+
+function ParseMarkup(d, level = 0) {
+    result = '';
+    for (let item in d) {
+        item = d[item];
+        console.log('  '.repeat(level) + `Type: ${item.type}, Direction: ${item.direction}`);
+        local_end = '';
+        if (item.type == 'body') {
+            result += "<div class=\"container htmlpanel-body\">";
+            if (item.params.panel_width) { panel_element.style.width = item.params.panel_width; }
+            if (item.params.panel_height) { panel_element.style.height = item.params.panel_height; }
+            end = "</div>";
+        } else {
+            result += "<div class=\"panelitem ";
+            end = "</div>";
+            if (item.direction == 'row') {
+                result += "row"
+            } else if (item.direction == 'column') {
+                result += "col"
+            } else {
+                result += "item"
+            }
+            result += "\" style=\"";
+            if (item.params.color) { result += "background: " + item.params.color + ";"; }
+            if (item.params.border) {
+                if (['left', 'right', 'top', 'bottom'].includes(item.params.border)) {
+                    result += "border-" + item.params.border + ": 1px #999 solid;";
+                } else if (item.params.border == 'all') {
+                    result += "border: 1px #999 solid;";
+                }
+            }
+            if (item.params.fontSize) { result += "font-size: " + item.params.fontSize + ";"; }
+            if (item.params.underline) { result += "text-decoration: underline;"; }
+            if (item.params.bold) { result += "font-weight: bold;"; }
+            result += '\">';
+            if (item.type == 'checkbutton') {
+                result += '<button class="checkbutton" onclick="' + item.params.onclick + '"></button>';
+            }
+            if (item.type == 'checkmark') {
+                result += '<input type="checkbox" onchange="' + item.params.onclick + '">';
+            }
+            if (item.type == 'button') {
+                result += '<button class="button" onclick="' + item.params.onclick + '">' + item.params.text + '</button>';
+            }
+            if (item.params.text && item.type != 'input' && item.type != 'button') {
+                // result += item.params.text;
+                result += '<span>' + item.params.text + '</span>';
+            }
+            result += local_end;
+            if (item.type == 'inpinputut') {
+                result += '<input placeholder="' + item.params.text + ':" onkeyup="' + item.params.onclick + '" type="text">';
+            }
+        }
+        if (item.children.length > 0) {
+            console.log('  '.repeat(level) + `Parsing children...`);
+            result += ParseMarkup(item.children, level + 1);
+        }
+        result += end;
+    }
+    return result;
+}
+
+code = formatHTML(ParseMarkup(markup))
+console.log(code)
+panel_body = $('.htmlpanel')[0]
+panel_body.innerHTML += code
---
+++ main.js
@@ -0,0 +1,96 @@
+const sleep = ms => new Promise(r => setTimeout(r, ms));
+
+function dragElement(mainEl, dragEl) {
+    console.log('Drag invoked for:');
+    console.log(mainEl);
+    console.log(dragEl);
+
+    var pos1 = 0,
+        pos2 = 0,
+        pos3 = 0,
+        pos4 = 0;
+    dragEl.onmousedown = dragMouseDown;
+
+    function dragMouseDown(e) {
+        console.log('Drag event started');
+        e = e || window.event;
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        let new_top = (mainEl.offsetTop - pos2);
+        mainEl.style.top = new_top + "px";
+        let new_left = (mainEl.offsetLeft - pos1);
+        mainEl.style.left = new_left + "px";
+    }
+
+    function closeDragElement() {
+        console.log('Drag event set new top: ' + mainEl.style.top + ', left: ' + mainEl.style.left);
+        console.log('Drag event finished');
+        // stop moving when mouse button is released:
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
+
+function formatHTML(html) {
+    var tab = '\t';
+    var result = '';
+    var indent = '';
+
+    html.split(/>\s*</).forEach(function(element) {
+        if (element.match(/^\/\w/)) {
+            indent = indent.substring(tab.length);
+        }
+
+        result += indent + '<' + element + '>\r\n';
+
+        if (element.match(/^<?\w[^>]*[^\/]$/) && !element.startsWith("input")) {
+            indent += tab;
+        }
+    });
+
+    return result.substring(1, result.length - 3);
+}
+
+function minimizeWindow(element) {
+    let panel_body = $(element).find('.htmlpanel-body')[0];
+    if (panel_body.style.display != 'none') {
+        panel_body.style.display = 'none';
+    } else {
+        panel_body.style.display = '';
+    }
+}
+
+function closeWindow(element) {
+    $(element)[0].style.display = 'none';
+}
+
+function showWindow(element) {
+    $(element)[0].style.display = '';
+}
+
+function titleWindow(element, value) {
+    let panel_head = $(element).find('header');
+    panel_head.find('.drag-area')[0].innerHTML = value;
+}
+
+async function highlight(el, time) {
+    $(el).addClass('glow');
+    await sleep(time * 1000);
+    $(el).removeClass('glow');
+}
